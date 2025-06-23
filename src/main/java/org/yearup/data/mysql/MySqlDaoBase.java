51999d15a62bad9f8dfd7890d3d4c35ae9d8a0e8
Remsey Mailjard <35072380+RemseyMailjard@users.noreply.github.com>
2025-06-23 19:36:33 +0200

    Updated MySQL

--- src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
+++ src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
@@ -4,10 +4,21 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+package org.yearup.data.mysql;
+
+import org.springframework.beans.factory.annotation.Autowired; // Importeren
+import org.springframework.stereotype.Component; // Zorg ervoor dat de subklassen dit hebben
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public abstract class MySqlDaoBase
 {
     private DataSource dataSource;
 
+    // Gebruik @Autowired op de constructor om de auto-geconfigureerde DataSource te ontvangen
+    @Autowired
     public MySqlDaoBase(DataSource dataSource)
     {
         this.dataSource = dataSource;
